cf096e02eb6a8c9b8bea83552919df3ef73cd3ed
snrteftelya <snrteftelya@gmail.com>
2025-05-02 14:50:34 +0300

    lab5

--- src/main/java/org/example/model/Nation.java
+++ src/main/java/org/example/model/Nation.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.util.List;
 import lombok.Data;
